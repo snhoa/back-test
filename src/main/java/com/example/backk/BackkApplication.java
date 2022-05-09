4ce0f5e6c10d948f05670a591ece4ad20f235832
odnac <cancode777@gmail.com>
2022-05-09 21:18:28 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/backk/BackkApplication.java
@@ -0,0 +1,13 @@
+package com.example.backk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backk/BackkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
