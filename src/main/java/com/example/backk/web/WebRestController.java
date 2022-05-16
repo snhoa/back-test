db3c487a72b2f7174af5387fc6fa26b48e7bd8b8
odnac <cancode777@gmail.com>
2022-05-16 12:55:04 +0900

    2-2 SpringBoot & JPA

--- src/main/java/com/example/backk/web/WebRestController.java
+++ src/main/java/com/example/backk/web/WebRestController.java
@@ -3,6 +3,9 @@ package com.example.backk.web;
 import com.example.backk.domain.posts.PostsRepository;
 import com.example.backk.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MetricsProperties.Web.class);
     private PostsRepository postsRepository;
 
     @GetMapping("/hello")
@@ -21,6 +25,7 @@ public class WebRestController {
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        logger.info(dto.toString());
         postsRepository.save(dto.toEntity());
     }
 }
