944512e4e10a6381847860ecf302a39687683b40
snhoa <snhoa16@naver.com>
2022-05-25 16:50:14 +0900

    3

--- src/main/java/com/example/backk/web/WebRestController.java
+++ src/main/java/com/example/backk/web/WebRestController.java
@@ -2,6 +2,7 @@ package com.example.backk.web;
 
 import com.example.backk.domain.posts.PostsRepository;
 import com.example.backk.dto.posts.PostsSaveRequestDto;
+import com.example.backk.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MetricsProperties.Web.class);
-    private PostsRepository postsRepository;
+    //private static final Logger logger = LoggerFactory.getLogger(MetricsProperties.Web.class);
+    //private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -25,7 +27,8 @@ public class WebRestController {
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-        logger.info(dto.toString());
-        postsRepository.save(dto.toEntity());
+        //logger.info(dto.toString());
+       //postsRepository.save(dto.toEntity());
+        postsService.save(dto);
     }
 }
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+insert into posts (title, author, content, created_date, modified_date) values ('테스트1', 'devzec', '테스트1의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('테스트2', 'devzec', '테스트2의 본문', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('테스트3', 'asd', '테스트3의 본문', now(), now());
